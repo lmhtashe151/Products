8f80078bc08b699897ecc3924860abadaac00dab
lmhtashe151 <quynd1501@gmail.com>
2023-07-15 09:59:41 +0700

    first commit

---
+++ header.php
@@ -0,0 +1,15 @@
+<header class="header">
+
+   <div class="flex">
+
+      <a href="#" class="logo">foodies</a>
+
+      <nav class="navbar">
+         <a href="admin.php">add products</a>
+         <a href="login.php">logout</a>
+      </nav>
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+   </div>
+
+</header>
---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.onscroll = () =>{
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
+};
+
+
+document.querySelector('#close-edit').onclick = () =>{
+   document.querySelector('.edit-form-container').style.display = 'none';
+   window.location.href = 'admin.php';
+};
